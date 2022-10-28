75a0bdb57573ab03901afcbf48a508069f1da8c0
loleceta1 <cecilialorenatarantelli@gmail.com>
2022-10-28 13:00:46 -0300

    rectificacion de validacionesJs

--- js/reservas.js
+++ js/reservas.js
@@ -2,23 +2,33 @@
 function validar1() {
   
 	const email = document.getElementById("email").value;
+	const comment = document.getElementById("comment").value;
 	let bandera = 1;
 	
+	
 	if(email==="") {
-		alert("Debe completar los datos");
+		alert("Debe ingresar un correo");
 		return false;
-		bandera=0;
+		bandera = 0;
+		
 	}
+
 	 let re=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
 	if(!re.exec(email)){
 		alert('¡¡¡E-mail no válido!!!');
 		return false;
-		bandera=0;
-	}
-	
-	if(bandera===1){
-		alert("Datos Enviados");
-		return false;
+		bandera = 0;
 		
+	}
+
+	if(comment==="") {
+		alert("Nos gustaría conocer tu opinión");
+		return false;
+		bandera = 0;
+		
+	}
+		
+	if(bandera===1){
+		alert("Te contactaremos a la brevedad . Muchas Gracias !!!");
 	}
 }
